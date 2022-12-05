b2ca26000e4b394c358f4af7dd525a5139730e07
fabagile <fabagile@outlook.fr>
2022-12-05 09:17:05 +0100

    links:
     - link main apps
     - redirect other anchors

--- src/app/pages/portfolio/releases.ts
+++ src/app/pages/portfolio/releases.ts
@@ -8,35 +8,72 @@ export const RELEASES = [
         url: 'https://pklinux.netlify.app/',
         detailPath: 'pklinux',
       },
-    ],
-  },
-  {
-    category: 'Sites créés en formation',
-    items: [
       {
-        img: 'assets/pictures/portfolio/voyage.png',
-        label: 'Travel Agency',
-        detailPath: 'https://fabric-io.netlify.app/portfolio/travel-agency',
-        url: 'https://travelagency-mod.netlify.app/',
+        img: 'assets/pictures/portfolio/scrnli - aloe44.png',
+        label: 'aloe 44',
+        detailPath: 'https://fabric-io.netlify.app/portfolio/aloe44',
+        url: 'https://aloe44-demo.vercel.app/',
       },
       {
-        img: 'assets/pictures/portfolio/serpent.png',
-        label: 'Snakeey',
-        detailPath: 'https://fabric-io.netlify.app/portfolio/snakeey',
-        url: 'https://snakeey.netlify.app/',
-      },
-      {
-        img: 'assets/pictures/portfolio/color-image.jpg',
-        label: 'Top 5 Actrices',
-        detailPath: 'https://fabric-io.netlify.app/portfolio/actrices',
-        url: 'https://topactrices.netlify.app/',
-      },
-      {
-        img: 'assets/pictures/portfolio/banner-overlay1.png',
-        label: 'Do-Pe',
-        detailPath: 'https://fabric-io.netlify.app/portfolio/dope',
-        url: 'https://do-pe.netlify.app/',
+        img: 'assets/pictures/portfolio/scrnli - todolist.png',
+        label: 'todolist',
+        detailPath: 'https://fabric-io.netlify.app/portfolio/pokedex',
+        url: 'https://todolist-demo.vercel.app/',
       },
     ],
   },
+  // {
+  //   category: 'Sites créés en formation',
+  //   items: [
+  //     {
+  //       img: 'assets/pictures/portfolio/voyage.png',
+  //       label: 'Travel Agency',
+  //       detailPath: 'https://fabric-io.netlify.app/portfolio/travel-agency',
+  //       url: 'https://travelagency-mod.netlify.app/',
+  //     },
+  //     {
+  //       img: 'assets/pictures/portfolio/serpent.png',
+  //       label: 'Snakeey',
+  //       detailPath: 'https://fabric-io.netlify.app/portfolio/snakeey',
+  //       url: 'https://snakeey.netlify.app/',
+  //     },
+  //     {
+  //       img: 'assets/pictures/portfolio/color-image.jpg',
+  //       label: 'Top 5 Actrices',
+  //       detailPath: 'https://fabric-io.netlify.app/portfolio/actrices',
+  //       url: 'https://topactrices.netlify.app/',
+  //     },
+  //     {
+  //       img: 'assets/pictures/portfolio/banner-overlay1.png',
+  //       label: 'Do-Pe',
+  //       detailPath: 'https://fabric-io.netlify.app/portfolio/dope',
+  //       url: 'https://do-pe.netlify.app/',
+  //     },
+
+  //     {
+  //       img: 'assets/pictures/portfolio/scrnli - findy.png',
+  //       label: 'findy',
+  //       detailPath: 'https://fabric-io.netlify.app/portfolio/findy',
+  //       url: 'https://findy.netlify.app/',
+  //     },
+  //     {
+  //       img: 'assets/pictures/portfolio/scrnli - magic music.png',
+  //       label: 'magic music',
+  //       detailPath: 'https://fabric-io.netlify.app/portfolio/magicmusic',
+  //       url: 'https://magicmusic.netlify.app/',
+  //     },
+  //     {
+  //       img: 'assets/pictures/portfolio/scrnli - rick & morty.png',
+  //       label: 'jeu rick & morty',
+  //       detailPath: 'https://fabric-io.netlify.app/portfolio/pokedex',
+  //       url: 'https://rick-morty-game.netlify.app/',
+  //     },
+  //     {
+  //       img: 'assets/pictures/portfolio/scrnli - pokedex  - top.png',
+  //       label: 'pokedex',
+  //       detailPath: 'https://fabric-io.netlify.app/portfolio/pokedex',
+  //       url: 'https://agile-pokedex.vercel.app/',
+  //     },
+  //   ],
+  // },
 ];
