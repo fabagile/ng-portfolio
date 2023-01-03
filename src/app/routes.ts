76e0d591301f79d346525b323062c33eee4225ed
fabagile <114162026+fabagile@users.noreply.github.com>
2023-01-03 10:42:24 +0100

    routes.ts: erase component field

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,40 +1,31 @@
-import { AboutComponent } from './pages/main/about/about.component';
-import { ContactComponent } from './pages/footer/contact/contact.component';
-import { CguComponent } from './pages/footer/components/cgu/cgu.component';
-import { FaqComponent } from './pages/footer/faq/faq.component';
-import { HelpComponent } from './pages/footer/help/help.component';
-import { MapComponent } from './pages/footer/map/map.component';
-import { LandingPageComponent } from './home/landing-page.component';
-import { PortfolioComponent } from './pages/main/portfolio/portfolio.component';
-import { ServicesComponent } from './pages/main/components/supplies/services.component';
-
 export const MAIN_LINKS = [
   {
     path: '',
     label: 'accueil',
-    component: LandingPageComponent,
     desc: 'Page de garde',
     icon: 'house',
   },
   {
     path: '/services',
     label: 'services',
-    component: ServicesComponent,
     desc: 'Les services proposés',
     icon: 'user-tie',
   },
-  // {path:"/skills", label: "compétences", component: SkillsComponent, desc:"Le savoir-faire de l'auteur", icon: "graduation-cap"},
+  // {
+  //   path: '/skills',
+  //   label: 'compétences',
+  //   desc: "Le savoir-faire de l'auteur",
+  //   icon: 'graduation-cap',
+  // },
   {
     path: '/portfolio',
     label: 'portfolio',
-    component: PortfolioComponent,
     desc: "les créations de l'auteur",
     icon: 'tasks',
   },
   {
     path: '/about',
     label: 'à propos',
-    component: AboutComponent,
     desc: 'qui suis-je?',
     icon: 'person-circle-question',
   },
@@ -43,35 +34,30 @@ export const FOOTER_LINKS = [
   {
     path: '/footer/map',
     label: 'plan du site',
-    component: MapComponent,
     desc: 'la structure du site',
     icon: 'sitemap',
   },
   {
     path: '/footer/contact',
     label: 'contactez-moi',
-    component: ContactComponent,
     desc: "contacter l'auteur",
     icon: 'envelope',
   },
   {
     path: '/footer/cgu',
     label: 'conditions',
-    component: CguComponent,
     desc: "les conditions générales d'utilisation",
     icon: 'map',
   },
   {
     path: '/footer/help',
     label: 'aide',
-    component: HelpComponent,
     desc: "Un peu d'aide ne fait de mal à personne",
     icon: 'circle-question',
   },
   {
     path: '/footer/faq',
     label: 'foire aux questions',
-    component: FaqComponent,
     desc: 'Des questions? Quelques réponses',
     icon: 'question',
   },
