a7c1c862c6b72d5ac750611af41595aa61c1746a
fabagile <114162026+fabagile@users.noreply.github.com>
2023-01-03 16:23:25 +0100

    subscriber log handle

--- src/app/core/components/layout/header/header.component.ts
+++ src/app/core/components/layout/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'layout-header',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  logCommand!:string
+  logBtnColor = "info"
+   active=false
 
-  constructor() { }
+  constructor(private router:Router
+    ) { }
 
   ngOnInit(): void {
+    this.logCommand = "connexion"
+  }
+  handleLogStatus(){
+    if (this.logCommand != "connexion") {
+      this.router.navigateByUrl("/")
+      this.logCommand = "connexion"
+      this.logBtnColor = "secondary"
+    }
+    else {
+      this.router.navigateByUrl("/subscribers/log-in")
+      this.logCommand = "déconnexion"
+      this.logBtnColor = "info"
+    }
   }
 
 }
--- src/app/core/components/layout/navbar/navbar.component.ts
+++ src/app/core/components/layout/navbar/navbar.component.ts
@@ -1,23 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { MAIN_LINKS } from 'src/app/routes';
 // import { routes } from 'src/app/routes';
 
 @Component({
   selector: 'layout-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-  collapse=true
-  links = MAIN_LINKS
-  active=false
-  toggleCollapse(){
-    this.collapse = !this.collapse
+  collapse = true;
+  links = MAIN_LINKS;
+  active = false;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+  toggleCollapse() {
+    this.collapse = !this.collapse;
   }
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
